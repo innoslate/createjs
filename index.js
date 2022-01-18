3e330c8a3a2979725c171e821d14944d2a11c6ba
Youness Bella <youness.bella@specinnovations.com>
2022-01-18 10:37:49 -0500

    Removed deprecated warning

--- index.js
+++ index.js
@@ -228,8 +228,6 @@ createjs.indexOf = function (array, searchElement){
 createjs.deprecate = function(fallbackMethod, name) {
   "use strict";
   return function() {
-    var msg = "Deprecated property or method '"+name+"'. See docs for info.";
-    console && (console.warn ? console.warn(msg) : console.log(msg));
     return fallbackMethod && fallbackMethod.apply(this, arguments);
   }
 };
